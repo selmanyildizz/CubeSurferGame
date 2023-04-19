709afc2233d28df21828f5a34b1540455496a828
yasin-herken <yasin.herken123@gmail.com>
2023-04-20 02:00:52 +0300

    Camera following is ready

--- Assets/Scripts/Camera/TracingCamera.cs
+++ Assets/Scripts/Camera/TracingCamera.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class TracingCamera : MonoBehaviour
 {
 
-    public Transform target;
-    public float smoothSpeed = 0.125f;
+    public GameObject target;
+    [SerializeField] private float smoothSpeed = 0.5f;
+
+    public void setSmoothSpeed(float value)
+    {
+        smoothSpeed = value;
+    }
+
+    private Movement movement;
 
     private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+                                       // Use this for initialization
 
-    // Use this for initialization
-    void Start()
+    private void Awake()
     {
+        setSmoothSpeed(target.GetComponent<Movement>().getForwardSpeed());
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - target.transform.position;
     }
+    void Start()
+    {
+        
+        Debug.Log(smoothSpeed);
+    }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        transform.position = target.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * smoothSpeed);  
     }
 }
--- Assets/Scripts/Character/Movement.cs
+++ Assets/Scripts/Character/Movement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float forwardSpeed = 0.5f;
+    
     [SerializeField] private float leftRightMovementSpeed;
 
+
+    public float getForwardSpeed()
+    {
+        return forwardSpeed;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +24,6 @@ public class Movement : MonoBehaviour
     void Update()
     {
         float HorizontalAxis = Input.GetAxis("Horizontal") * leftRightMovementSpeed * Time.deltaTime;
-        this.transform.Translate(HorizontalAxis, 0, forwardSpeed * Time.deltaTime);
+        this.transform.Translate(HorizontalAxis, 0, getForwardSpeed() * Time.deltaTime);
     }
 }
